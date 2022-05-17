dba05c479a4c6388bb9202cdc53e3cfbc8890181
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-17 17:22:26 -0300

    removendo arquivos de teste

--- src/app/modules/menu/model/list-item-menu.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ListItemMenu } from './list-item-menu';
-
-describe('ListItemMenu', () => {
-  it('should create an instance', () => {
-    expect(new ListItemMenu()).toBeTruthy();
-  });
-});
--- src/app/websocket/websocketconnector.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WebSocketConnector } from './websocketconnector';
-
-describe('Websocketconnector', () => {
-  it('should create an instance', () => {
-    expect(new WebSocketConnector()).toBeTruthy();
-  });
-});
