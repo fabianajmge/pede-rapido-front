e4ca2d1f183ce7b1221e0b67e884c5e5f095ad63
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-20 11:49:02 -0300

    link prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  URL_API_PEDIDO: 'http://localhost:8080',
+  URL_API_PEDIDO: 'http://3.83.92.209:8080',
 
   cognito: {
     userPoolId: 'us-east-1_0sVdlrSMk',
