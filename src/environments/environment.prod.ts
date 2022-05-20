db52c661a74913539b8221fee1b4401d3e18ef17
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-20 15:49:09 -0300

    url api gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
 
-  URL_API_PEDIDO: 'http://localhost:8084',
+  //URL_API_PEDIDO: 'http://localhost:8084',
+  URL_API_PEDIDO: 'https://cxf92u0wzd.execute-api.us-east-1.amazonaws.com/gatewaypederapido';
 
   cognito: {
     userPoolId: 'us-east-1_0sVdlrSMk',
